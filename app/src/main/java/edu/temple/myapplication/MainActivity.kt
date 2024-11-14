5e1fc7f6f8ef91c4d033398b417e106121323310
shilohcoconuts <shilohpalmer57@gmail.com>
2024-11-14 14:52:25 -0500

    functionality enabled without ui state change

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,65 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnection = false
+    var buttonState = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        val serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                timerBinder = service as TimerService.TimerBinder
+                isConnection = true
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                isConnection = false
+
+            }
+        }
+
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+
+            if (buttonState) {
+                startButton.text = "start"
+                if (isConnection) {
+                    timerBinder.pause()
+                }
+                buttonState = false
+            } else {
+                startButton.text = "Pause"
+                if (isConnection) {
+                    timerBinder.start(100)
+                }
+                buttonState = true
+            }
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (isConnection) {
+                timerBinder.stop()
+            }
         }
     }
 }
